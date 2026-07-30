63a96a533fa3cad14d21671d5528e1162a4dbd6c
Justin Hutzler <justin@synergy-network.io>
2026-07-30 17:29:45 -0500

    ops(chain1266): authorize verifier-cache runtime

--- scripts/stage-testnet-v3-v20-runtime-hotfix.sh
+++ scripts/stage-testnet-v3-v20-runtime-hotfix.sh
@@ -81,7 +81,7 @@ done
 
 source_revision=$(tr -d '[:space:]' < "$artifact_dir/TESTNET_SOURCE_REVISION")
 [[ $source_revision =~ ^[0-9a-f]{40}$ ]] || fail 'artifact source revision is invalid'
-[[ $source_revision == d373e778f683dbf96d638736adcef89e0d127951 ]] ||
+[[ $source_revision == fd3b3e4d882b17e3393a13645984f044fdedc32b ]] ||
   fail "artifact is not bound to the authorized Testnet-v3 source revision: $source_revision"
 
 checksum_entries=0
--- scripts/testnet-v3-v20-runtime-hotfix-remote.sh
+++ scripts/testnet-v3-v20-runtime-hotfix-remote.sh
@@ -45,7 +45,7 @@ done
 [[ $expected_binary_sha256 =~ ^[0-9a-f]{64}$ ]] || fail 'binary SHA-256 is invalid'
 [[ $expected_genesis_sha256 =~ ^[0-9a-f]{64}$ ]] || fail 'Genesis SHA-256 is invalid'
 [[ $source_revision =~ ^[0-9a-f]{40}$ ]] || fail 'source revision is invalid'
-[[ $source_revision == d373e778f683dbf96d638736adcef89e0d127951 ]] ||
+[[ $source_revision == fd3b3e4d882b17e3393a13645984f044fdedc32b ]] ||
   fail 'source revision is not the authorized Testnet-v3 release'
 
 staged_binary="$stage_dir/runtime.bin"
