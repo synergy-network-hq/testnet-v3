997b4c6f14d4d90fbde7a8fc97b3a77e1e3cea4d
Justin Hutzler <justin@synergy-network.io>
2026-07-30 10:30:39 -0500

    ops: authorize prepared recovery runtime

--- scripts/testnet-v3-v20-runtime-hotfix-remote.sh
+++ scripts/testnet-v3-v20-runtime-hotfix-remote.sh
@@ -45,7 +45,7 @@ done
 [[ $expected_binary_sha256 =~ ^[0-9a-f]{64}$ ]] || fail 'binary SHA-256 is invalid'
 [[ $expected_genesis_sha256 =~ ^[0-9a-f]{64}$ ]] || fail 'Genesis SHA-256 is invalid'
 [[ $source_revision =~ ^[0-9a-f]{40}$ ]] || fail 'source revision is invalid'
-[[ $source_revision == 24c274facb7c85d9eb9032d49c6468daa9117c6d ]] ||
+[[ $source_revision == 22bb19a06ee6bf83945aaac4f3490385b182b184 ]] ||
   fail 'source revision is not the authorized Testnet-v3 release'
 
 staged_binary="$stage_dir/runtime.bin"
