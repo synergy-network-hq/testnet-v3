c5cbcb9a2214c0beae0cbc1a9688ffda05505dc8
Justin Hutzler <justin@synergy-network.io>
2026-07-30 15:43:27 -0500

    ops(testnet-v3): authorize remote vote rebroadcast rollout

--- scripts/testnet-v3-v20-runtime-hotfix-remote.sh
+++ scripts/testnet-v3-v20-runtime-hotfix-remote.sh
@@ -45,7 +45,7 @@ done
 [[ $expected_binary_sha256 =~ ^[0-9a-f]{64}$ ]] || fail 'binary SHA-256 is invalid'
 [[ $expected_genesis_sha256 =~ ^[0-9a-f]{64}$ ]] || fail 'Genesis SHA-256 is invalid'
 [[ $source_revision =~ ^[0-9a-f]{40}$ ]] || fail 'source revision is invalid'
-[[ $source_revision == d76dbf3ef315beca3a31974f7785d76b14e6e71c ]] ||
+[[ $source_revision == 606ec51f303b0cf19843f9da257d15ef38186681 ]] ||
   fail 'source revision is not the authorized Testnet-v3 release'
 
 staged_binary="$stage_dir/runtime.bin"
