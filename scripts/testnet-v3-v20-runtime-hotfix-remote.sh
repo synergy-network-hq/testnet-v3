42a6a5ed083b641c0077835f4488473de4d69178
Justin Hutzler <justin@synergy-network.io>
2026-07-30 12:22:37 -0500

    ops(testnet-v3): authorize checkpoint recovery runtime

--- scripts/testnet-v3-v20-runtime-hotfix-remote.sh
+++ scripts/testnet-v3-v20-runtime-hotfix-remote.sh
@@ -45,7 +45,7 @@ done
 [[ $expected_binary_sha256 =~ ^[0-9a-f]{64}$ ]] || fail 'binary SHA-256 is invalid'
 [[ $expected_genesis_sha256 =~ ^[0-9a-f]{64}$ ]] || fail 'Genesis SHA-256 is invalid'
 [[ $source_revision =~ ^[0-9a-f]{40}$ ]] || fail 'source revision is invalid'
-[[ $source_revision == 6de99e5992735d04a6a5c29c6ffb26de1984e6f3 ]] ||
+[[ $source_revision == 8531829656b7cbd6105eafaffc9569a8e85c49e9 ]] ||
   fail 'source revision is not the authorized Testnet-v3 release'
 
 staged_binary="$stage_dir/runtime.bin"
