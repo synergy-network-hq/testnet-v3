86191d144453d27f3502b207d646c585dcae17d3
Justin Hutzler <justin@synergy-network.io>
2026-07-30 09:35:02 -0500

    ops: authorize 24c274f round-change fix runtime (run 30552010319)

--- scripts/stage-testnet-v3-v20-runtime-hotfix.sh
+++ scripts/stage-testnet-v3-v20-runtime-hotfix.sh
@@ -73,7 +73,7 @@ done
 
 source_revision=$(tr -d '[:space:]' < "$artifact_dir/TESTNET_SOURCE_REVISION")
 [[ $source_revision =~ ^[0-9a-f]{40}$ ]] || fail 'artifact source revision is invalid'
-[[ $source_revision == 1b92a065fbe7ab5e9b87c2b2298e8a7898d0d503 ]] ||
+[[ $source_revision == 24c274facb7c85d9eb9032d49c6468daa9117c6d ]] ||
   fail "artifact is not bound to the authorized Testnet-v3 source revision: $source_revision"
 
 checksum_entries=0
